74e8e827c88ee5dee3d4a8550d05189f4429f396
Francesca Capuzzo <f.capuzzo9966@gmail.com>
2022-01-21 13:26:38 +0100

    u

---
+++ fibonacci.js
@@ -0,0 +1,29 @@
+
+// function fibonacci2(number = 0) {
+//     for (let i = 0; i <= 100; i++) {
+//         let counter = 0
+
+//         if (number === 0){
+//         return 0;
+//     }
+//         if (number === 1){
+//         return 1;
+//     }
+// }     
+// return fibonacci2(number-1) + fibonacci2(number-2)     
+// }
+
+// console.log(fibonacci2(10));
+
+
+function fibonacci(number) {
+    let n1 = 0;
+    let n2= 1;
+    let j = 1;
+    for (let i = 0; i <= 100; i++) {
+        let j = n1 + n2
+        return result
+    }
+}
+
+console.log(fibonacci(10));
